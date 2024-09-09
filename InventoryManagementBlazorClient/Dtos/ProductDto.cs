442151bc00e9b9fcf995c6a021823273e9215a90
andrenormanlang <andrernormanlang@gmail.com>
2024-09-09 23:10:09 +0200

    added proper validation

--- InventoryManagementBlazorClient/Dtos/ProductDto.cs
+++ InventoryManagementBlazorClient/Dtos/ProductDto.cs
@@ -1,13 +1,34 @@
-﻿namespace InventoryManagementBlazorClient.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagementBlazorClient.Dtos
 {
     public class ProductDto
     {
         public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(100, ErrorMessage = "Product name cannot be longer than 100 characters.")]
+        [MinLength(5, ErrorMessage = "Product name must be at least 5 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Product description is required.")]
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
+        [MinLength(50, ErrorMessage = "Description must be at least 50 characters.")]
         public string Description { get; set; }
+
+        [Range(5.00, double.MaxValue, ErrorMessage = "Price must be greater than 5.")]
         public double Price { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
         public int Quantity { get; set; }
-        public int CategoryId { get; set; }  // Only CategoryId is needed
-        public int SupplierId { get; set; }  // Only SupplierId is needed
+
+        [Required(ErrorMessage = "Category is required.")]
+        public string? CategoryName { get; set; }
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Supplier is required.")]
+        public string? SupplierName { get; set; }
+        public int SupplierId { get; set; }
     }
 }
+
